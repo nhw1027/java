359564c3d1fc4e668df507cddfb9b52172ff05b1
nhw1027 <nhwee1027@gmail.com>
2021-05-03 14:17:57 +0900

    test

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package java_20210429;
+
+//폰트 바꾸는 방법
+// 1. window -> Preferences
+// 2. font 입력한 후에 Java -> Java Text Editor Font ~~
+// 		클릭하여 글자크기를 변경한다
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
